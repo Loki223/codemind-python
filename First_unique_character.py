fa167ff2c61dec07c56f96021162d26bb34ae3d9
Loki223 <96326944+Loki223@users.noreply.github.com>
2022-12-10 19:21:50 +0530

    s=input() s=s.lower() a=[] for i in s:     c=0     for j in s:         if i.isalnum()==True and j.isalnum()==True:             if i==j:                 c+=1     if c==1:         a.append(i) if a==[]:     print(-1) else:     print(a[0])

---
+++ First_unique_character.py
@@ -0,0 +1,15 @@
+s=input()
+s=s.lower()
+a=[]
+for i in s:
+    c=0
+    for j in s:
+        if i.isalnum()==True and j.isalnum()==True:
+            if i==j:
+                c+=1
+    if c==1:
+        a.append(i)
+if a==[]:
+    print(-1)
+else:
+    print(a[0])
