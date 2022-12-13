b585fe6843bec97746afac7b33ec24aa67bf61f2
Loki223 <96326944+Loki223@users.noreply.github.com>
2022-12-13 19:15:26 +0530

    import math def prime(a):     if a<2:         return False     for i in range(2,int(math.sqrt(a)+1)):         if a%i==0:             return False     return True n=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if prime(i)==True and i<=k:         c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,16 @@
+import math
+def prime(a):
+    if a<2:
+        return False
+    for i in range(2,int(math.sqrt(a)+1)):
+        if a%i==0:
+            return False
+    return True
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in l:
+    if prime(i)==True and i<=k:
+        c+=1
+print(c)
