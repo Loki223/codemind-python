9b0fba852b462a376b85e13635420c429c47a7f6
Loki223 <96326944+Loki223@users.noreply.github.com>
2022-12-13 19:00:33 +0530

    n=int(input()) l=list(map(int,input().split())) if n%2==0:     f=l[0:(n//2)]     s=l[(n//2):len(l)+1] else:     f=l[0:(n//2)]     s=l[(n//2):len(l)+1] print(sum(f),sum(s),sep=" ")

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+if n%2==0:
+    f=l[0:(n//2)]
+    s=l[(n//2):len(l)+1]
+else:
+    f=l[0:(n//2)]
+    s=l[(n//2):len(l)+1]
+print(sum(f),sum(s),sep="
+")
