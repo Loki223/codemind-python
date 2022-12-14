416085919b63732cbb522d80ed519852bbf4661b
Loki223 <96326944+Loki223@users.noreply.github.com>
2022-12-14 15:06:02 +0530

    m,n=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l=[] for i in l1:     if i in l2 and i not in l:         l.append(i) print(*l)

---
+++ Common_elements.py
@@ -0,0 +1,8 @@
+m,n=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l=[]
+for i in l1:
+    if i in l2 and i not in l:
+        l.append(i)
+print(*l)
